98ffc4610b36fb55d8151bf55113183d14bb5573
Jonas Swiatek <jonas.swiatek@gmail.com>
2024-10-31 12:40:27 +0100

    Code

--- src/DecklistApiCdk/FunctionCode/react-router-cloudfront-function.js
+++ src/DecklistApiCdk/FunctionCode/react-router-cloudfront-function.js
@@ -1,7 +1,9 @@
 function handler(event) {
-    var request = event.request;
-
-    if (request.uri.startsWith('/events')) {
+    const request = event.request;
+    
+    if (request.uri.startsWith('/events') ||
+        request.uri.startsWith('/new-event'))
+    {
         request.uri = "/index.html";
         request.querystring = {}
     }
