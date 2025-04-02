636214c423e951222f1947a7718d8a79a40970df
Jonas Swiatek <jonas.swiatek@gmail.com>
2025-04-02 14:14:16 +0200

    Version

--- src/DecklistApiCdk/Program.cs
+++ src/DecklistApiCdk/Program.cs
@@ -11,7 +11,7 @@ namespace MtgDecklistsCdk
         public static readonly string DomainName = "decklist.lol";
 
         public static readonly string DecklistApiAotImageTag  = "DecklistApi.Web.Aot-6";
-        public static readonly string DecklistWebsiteVersion  = "v1.0.6";
+        public static readonly string DecklistWebsiteVersion  = "v1.0.7";
         public static readonly string ScryfalllReaderImageTag = "DecklistApi.ScryfallReader-1";
 
         public static void Main(string[] args)
